632f6468ab4ca357a0f4942482f2b7d25944ad8c
Santiago <bobadillasantiago00@gmail.com>
2025-11-21 21:56:17 -0300

    se agrega perfil nuevo, faltan retoques visuales

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from .views import ActivarEmprendimientoView, EmprendimientoView, RegistroUsuarioView, LoginUsuarioView, PerfilUsuarioView, CambiarPasswordView
 from django.contrib.auth.views import LogoutView
 
@@ -8,7 +8,7 @@ app_name="usuarios"
 urlpatterns = [
     path("registro/", RegistroUsuarioView.as_view(), name="registro"),
     path("login/", LoginUsuarioView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(next_page=reverse_lazy("usuarios:login")), name="logout"),
     path("perfil/", PerfilUsuarioView.as_view(), name="perfil"),
     path("seguridad/", CambiarPasswordView.as_view(), name="seguridad"),
     path("emprendimiento/", EmprendimientoView.as_view(), name="emprendimiento"),
